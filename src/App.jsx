6e335f2417ae0db8f14735fc1d24b902c7c9a678
TaahaaUsman <taha.usman.ccl@gmail.com>
2025-12-02 11:04:29 +0500

    Initial comment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Navbar, Home, About, Contact } from "./components";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+
+export { Navbar, About, Contact, Home };
